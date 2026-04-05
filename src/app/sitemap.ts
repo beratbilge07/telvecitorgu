46927464d6c37354f23d7c36887d125a340249fe
Berat Bilge <beratbilge07@gmail.com>
2026-04-05 15:39:00 +0300

    chore(seo): add legal pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/blog',
     '/iletisim',
     '/teklif-al',
+    '/kullanim-sartlari',
+    '/gizlilik-politikasi',
   ];
 
   corePages.forEach((path) => {
